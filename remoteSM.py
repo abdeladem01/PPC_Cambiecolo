0ef0e3d0478a0c133441ecdbcb62a9c6a959b7d2
Abdeladem Fattah <74178270+abdeladem01@users.noreply.github.com>
2022-01-13 10:57:33 +0000

    Remoted Shared Memory Manager & server completed

--- remoteSM.py
+++ remoteSM.py
@@ -1,43 +1,31 @@
 from multiprocessing.managers import BaseManager as MyManager
 from multiprocessing import Lock
-
-
+#Defining Remote Managed Class...
 class MyRemoteClass:
-
     def __init__(self, number):
         self.available = {}
         self.offer = {}
         self.mutex = Lock()
         self.bell=Lock()
-
     def get_flag(self):
         return self.available
-
     def set_flag(self, pushed_bool, key):
         self.available[key] = pushed_bool
-
     def get_offers(self):
         return self.offers
-
     def set_offers(self, pushed_list, key):
         self.offers[key] = pushed_list
-
-    def acquire_mutex(self):
+    def acquire_mutex(self): #Shared mutex on SM
         self.mutex.acquire()
-
-    def acquire_bell(self):
-        self.bell.acquire
-
     def release_mutex(self):
         self.mutex.release()
-
+    def acquire_bell(self): #Shared mutex on bell
+        self.bell.acquire
     def release_bell(self):
         self.bell.release()
 
-
-remote = MyRemoteClass()
-
-MyManager.register('sm', callable=lambda: remote)
-m = MyManager(address=("127.0.0.1", 8888), authkey=b'abracadabra')
-s = m.get_server()
-s.serve_forever()
+remote = MyRemoteClass() #Creating an object remote from this class
+MyManager.register('sm', callable=lambda: remote)  #registring the remote on shared memory with the manager class 
+m = MyManager(address=("127.0.0.1", 8888), authkey=b'abracadabra') #from the documentation, needs changes
+s = m.get_server() #return the actual server under the control of the Manager
+s.serve_forever() #start and run it forever
