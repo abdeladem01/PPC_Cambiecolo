bab34b367e5ece2cbe63cffe15dd75f4394dc1b6
Abdeladem Fattah <74178270+abdeladem01@users.noreply.github.com>
2022-01-18 18:47:46 +0000

    server up to date // need client

--- serverSM.py
+++ serverSM.py
@@ -3,9 +3,10 @@ import os
 import sys
 import random
 import sysv_ipc
-import signalt
+import signal
 import time
 import ascii_art
+import threading 
 
 
 key = 300
@@ -14,8 +15,11 @@ MyManager.register('sm')
 m = MyManager(address=("127.255.255.255", 20), authkey=b'abracadabra')
 m.connect()
 sm = m.sm()
-hands_items = {}
-deck=[]
+hands_items = {} #hands of player (will be needed one time only)
+deck=[] #deck of cards
+needToStop=True
+n=0 #nbr of players
+games=0 #nbr of games played
 
 def deck(): #Designating and Shuffling cards in deck
     global deck #deck() function has now acces to theglobal variable [String]deck
@@ -27,45 +31,79 @@ def deck(): #Designating and Shuffling cards in deck
     random.shuffle(deck)
     return deck
 
-def game():
+def game(thread):
     global deck
     global hands_items
-    n = int(input("Number n of players:"))
+    global needToStop
+    global n
+    global games
+    needToStop=True 
+    deck=[]
+    mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
+    if games==0:
+        n = int(input("Number n of players:"))
     deck = deck(n)
     hands_items = {}
     i, k = 0, 0
-    while i < n: 
-        pid, _ = mq.receive(type=1)
-        pid = int(pid.decode())
-        offers = sm.get_offers() #Disctionnary type
+    if state==0:
+        hands_items ={}
+        while i < n: 
+            pid, _ = mq.receive(type=1)
+            pid = int(pid.decode()) #receiving pid of player
+            offers = sm.get_offers() #Dictionnary type of offers
+            offers_items=[]
+            sm.set_offers(offers_items, pid) #{pid: []} starting an empty one
+            sm.set_flag(True, pid)
+            main = deck[k:k + 5]
+            hands_items[pid] = main
+            art=ascii_art.title()
+            toSend = art+"\nWelcome Player "+str(pid)+"! \n You already know the rules, so no need to explain them again :p \n You're now waiting for your friends, at least if you have them."
+            toSend = toSend.encode()
+            #Sending the pid of the server to each client so they have it they may need it to ring the bell
+            mq.send(toSend, type=pid)
+            pid_server=str(os.getpid()) # will send its owns pid (ppid variable in client file)
+            pid_server=pid_server.encode()
+            mq.send(pid_server, type=pid)
+            i,k=i+1,k+5
+        for pid, list in hands_items.items():
+            hand_pid = (' '.join(list)).encode() #send the initial hand for each client
+            mq.send(hand_pid, type=pid)
+        games+=1
+        needToStop=False
+        thread.start()
+    else:
+        offers = sm.get_offers() #Dictionnary type of offers
         offers_items=[]
-        sm.set_offers(offers_items, pid) #{pid: []}
-        sm.set_flag(True, pid)
-        main = deck[k:k + 5]
-        hands_items[pid] = main
-        art=ascii_art.title()
-        toSend = art+"\nWelcome Player "+str(pid)+"! \n You already know the rules, so no need to explain them again :p \n You're now waiting for your friends, at least if you have them."
-        toSend = toSend.encode()
-        #Sending the pid of the server to each client so they have it they may need it to ring the bell
-        mq.send(toSend, type=pid)
-        pid_server=str(os.getpid()) # will send its owns pid (ppid variable in client file)
-        pid_server=pid_server.encode()
-        mq.send(pid_server, type=pid)
-        i,k=i+1,k+5
-    for pid, list in hands_items.items():
-        hand_pid = (' '.join(map(str, list))).encode() #send the initial hand for each client
-        mq.send(hand_pid, type=pid)
-    signal.signal(signal.SIGUSR2, handler) #ToC SIGNAL USED
-    signal.pause() #panse and wait for a signal
+        for pid in offers.keys():
+            hand_pid = (' '.join(deck[k:k+5])).encode() #send the initial hand for each client
+            mq.send(hand_pid, type=pid)
+            k+=5
+            sm.set_offers(offers_items, pid) #{pid: []} starting an empty one
+            sm.set_flag(True, pid)
+    signal.signal(signal.SIGUSR2, handler)
+    signal.signal(signal.SIGINT, handler) 
+    signal.pause() #pause and wait for a signal
 
 #End signal handler (SIGUSR2)
 def handler(sig, frame):
-    if sig == signal.SIGUS24:
+    global needToStop
+    if sig == signal.SIGINT: 
+        needToStop=True
+        print("The game will be interrupted...")
+        for pid in hands_items.keys():
+            try:
+                os.kill(pid, signal.SIGTERM)
+            except OSError:
+                pass 
+        mq.remove()
+        sys.exit(1) 
+    if sig == signal.SIGUSR2:
         for pid in hands_items.keys():
             os.kill(pid, signal.SIGUSR2) #send SIGUSR2 to process's pid
     print("THE GAME IS OOOOVER!") #Change print to mq.send
     print("Recovering Scores Table")
     waitingStyle()
+    print("{USER : ACTUAL SCORE}")
     print(sm.get_points())
     While True:
         kp=input("Do you want to keep going? Put yes to play :) or anything to quit :(")
@@ -77,11 +115,16 @@ def handler(sig, frame):
         else:
             print(ascii_art.credits())
             print("See you soon!")
+            for pid in hands_items.keys():
+                try:
+                    os.kill(pid, signal.SIGTERM)
+                except OSError:
+                    pass
             mq.remove() #Quiting and removing the message queue
-            quit()
+            sys.exit(1)
 
 #Stylistic waiting
-def waitingStyle():
+def waitingStyle(): #to launch for moving points
     print("",end="")
     time.sleep(1) #do some work here...
     print("\r.  ",end="")
@@ -91,8 +134,29 @@ def waitingStyle():
     print("\r...",end="")
     time.sleep(1) #gratuitious amounts of work...
 
+
+def listenAndBan(): 
+    global needToStop
+    while True:
+        try:
+            pid, _ = mq.receive(type=1)
+            pid = int(pid.decode())
+            toSend="NO MORE PLACES"
+            mq.send(toSend.encode(), type=pid)
+            os.kill(pid,signal.SIGTERM)
+            if needToStop:
+                sys.exit(1)
+        except sysv_ipc.ExistentialError :
+            print("Closing Message Queue\nQuitting...")
+            break
+        except sysv_ipc.ExistentialError :
+            print("See you soon")
+            break
+
 if __name__ == "__main__":
+     mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
     pts=sm.get_points()
     for pid in hands_items.keys():
         sm.set_points(0,pid)
-    game()
+    thread = threading.Thread(target=listenAndBan) 
+    game() 
